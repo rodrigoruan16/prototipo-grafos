18779b40adde1547cf35ca12c521f1cf12682cc0
rruan16 <rodrigoruan16@hotmail.com>
2025-03-24 18:18:10 -0300

    add print

--- main.py
+++ main.py
@@ -11,14 +11,17 @@ def lerMatriz(fileName):
 
 # salva o nome da instância, quantidade de linhas e colunas em resultado.txt
 def salvarDados(instanceName, qtyLine, qtyColumn):
+    s = '{0} {1} {2}'.format(instanceName, qtyLine, qtyColumn)
     f = open('resultado.txt', 'w')
-    f.write('{0} {1} {2}'.format(instanceName, qtyLine, qtyColumn))
+    f.write(s)
     f.close()
+    print(s)
 
 def main(fileName):
     matriz = lerMatriz(fileName)
     line, col = matriz.shape
     salvarDados(fileName, line, col)
+
 
 if __name__ == '__main__':
     main(str(sys.argv[1]))
